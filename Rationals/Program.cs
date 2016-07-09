d30cd5ed0ae2ea3df5bdfe73c9133650d5337158
roiramot <roir@codevalue.net>
2016-07-09 21:00:13 +0300

    Add
    Add

---
+++ Rationals/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rationals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rational first = new Rational();
+            Rational second = new Rational();
+            first.Numerator = 2;
+            first.Denominator = 4;
+            second.Numerator = 4;
+            second.Denominator = 8;
+
+            Console.WriteLine("the numbers are:");
+            Console.WriteLine(first.ToString());
+            Console.WriteLine(second.ToString());
+        
+            Console.WriteLine();
+            Console.WriteLine("the decimal value is:");
+            Console.WriteLine(first.Decimalvalue);
+            Console.WriteLine(second.Decimalvalue);
+          
+            Console.WriteLine();
+            Console.WriteLine("the reduced rationals are:");
+            Console.WriteLine(first.ReduceRational().ToString());
+            Console.WriteLine(second.ReduceRational().ToString());
+
+            Console.WriteLine();
+            Console.WriteLine("the added rationals are:");
+            Console.WriteLine(first.AddRational(second).ToString());
+
+            Console.WriteLine();
+            Console.WriteLine("the Multiply of the rationals are:");
+            Console.WriteLine(first.MulRational(second).ToString());
+
+            Console.WriteLine();
+            Console.WriteLine("are the two rational equal");
+            Console.WriteLine(first.Equals(second));
+
+            Console.WriteLine();
+            Console.WriteLine("addition");
+            Rational addition = first + second;
+            Console.WriteLine(addition.ToString());
+
+            Console.WriteLine();
+            Console.WriteLine("subtruct");
+            Rational subtruct = first - second;
+            Console.WriteLine(subtruct.ToString());
+
+            Console.WriteLine();
+            Console.WriteLine("devide");
+            Rational devide = first / second;
+            Console.WriteLine(devide.ToString());
+
+            Console.WriteLine();
+            Console.WriteLine("multiply");
+            Rational multiply = first * second;
+            Console.WriteLine(multiply.ToString());
+            
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ Rationals/Rational.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rationals
+{
+    struct Rational
+    {
+        public bool Equals(Rational other)
+        {
+            return _denominator == other._denominator && _numerator == other._numerator;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return _denominator*_numerator;
+            }
+        }
+
+        private int _denominator;
+        private int _numerator;
+        public Rational(int numerator, int denominator)
+        {
+            _denominator = denominator;
+            if (numerator == 0)
+            {
+                _numerator = 1;
+            }
+            else
+            {
+                _numerator = numerator;
+            }
+        }
+
+        public Rational(int numerator) : this(numerator, 1)
+        {
+        }
+
+        public int Numerator
+        {
+            get { return _numerator; }
+            set { _numerator = value; }
+        }
+
+        public int Denominator
+        {
+            get { return _denominator; }
+            set { _denominator = value; }
+        }
+
+        public double Decimalvalue
+        {
+            get { return  (Convert.ToDouble(_numerator)/Convert.ToDouble(_denominator)); }
+            set { }
+            
+        }
+
+        public Rational AddRational(Rational first)
+        {
+            Rational newRational;
+            newRational._numerator = (first._numerator*this._denominator) + (this._numerator*first._denominator);
+            newRational._denominator = first._denominator*this._denominator;
+            return newRational;
+        }
+        public Rational SubtractRational(Rational first)
+        {
+            Rational newRational;
+            newRational._numerator = (first._numerator * this._denominator) - (this._numerator * first._denominator);
+            newRational._denominator = first._denominator * this._denominator;
+            return newRational;
+        }
+        public Rational MulRational(Rational first)
+        {
+            Rational newRational;
+            newRational._numerator = (first.ReduceRational()._numerator * this.ReduceRational()._numerator);
+            newRational._denominator = first.ReduceRational()._denominator * this.ReduceRational()._denominator;
+            return newRational;
+        }
+
+        public Rational DevideRational(Rational first)
+        {
+            Rational newRational;
+            newRational._numerator = (first.ReduceRational()._numerator * this.ReduceRational()._denominator);
+            newRational._denominator = first.ReduceRational()._denominator * this.ReduceRational()._numerator;
+            return newRational;
+        }
+
+        public Rational ReduceRational()
+        {
+            for (int i = 2; i < 50; i++)
+            {
+                if (this._numerator % i == 0 && this._denominator % i == 0)
+                {
+                    this._numerator = this._numerator / i;
+                    this.Denominator = this.Denominator / i;
+                    ReduceRational();
+                }
+            }
+            return this;
+        }
+
+        public override string ToString()
+        {
+
+            StringBuilder rationalToString = new StringBuilder();
+            rationalToString.Append(Numerator);
+            rationalToString.Append("/");
+            rationalToString.Append(_denominator);
+            return rationalToString.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+
+            Rational secondRational = obj is Rational ? (Rational) obj : new Rational();
+
+            this.ReduceRational();
+            secondRational.ReduceRational();
+            return (Numerator == secondRational.Numerator) && (Denominator == secondRational.Denominator);
+        }
+
+        public static Rational operator +(Rational lhs, Rational r)
+        {
+            return lhs.AddRational(r);
+        }
+        public static Rational operator -(Rational lhs, Rational r)
+        {
+            return lhs.SubtractRational(r);
+        }
+        public static Rational operator /(Rational lhs, Rational r)
+        {
+            return lhs.DevideRational(r);
+        }
+        public static Rational operator *(Rational lhs, Rational r)
+        {
+            return lhs.MulRational(r);
+        }
+    }
+}
